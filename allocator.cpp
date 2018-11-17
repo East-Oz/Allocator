762a1066c02f04321c3e7a18b944e77c92cf0ec4
Roman Surdukov <surdukov@gmail.com>
2018-11-17 14:32:42 +0300

    02 fixes

--- allocator.cpp
+++ allocator.cpp
@@ -23,13 +23,14 @@ struct Allocator
         using other = Allocator<U, MaxCount>;
     };
 
-    char* buffer    = nullptr;
-    size_t offset   = 0;
+    char*   m_buffer    = nullptr;
+    size_t  m_offset    = 0;
+    size_t  m_count     = 0;
 
     pointer allocate( std::size_t n )
     {
 #ifdef SHOW_DEBUG_INFO
-        std::cout << __PRETTY_FUNCTION__ << "[n = " << n << "]" << std::endl;
+        cout << __PRETTY_FUNCTION__ << "[n = " << n << "]" << endl;
 #endif
 
         if( n > MaxCount )
@@ -37,16 +38,24 @@ struct Allocator
             throw std::bad_alloc();
         }
 
-        if( buffer == nullptr )
-            buffer = static_cast<char*>(std::malloc( MaxCount * sizeof( value_type ) ));
+        if( m_buffer == nullptr )
+        {
+            m_buffer = static_cast<char*>(std::malloc( MaxCount * sizeof( value_type ) ));
+            m_count = MaxCount;
+#ifdef SHOW_DEBUG_INFO
+            cout << endl;
+            cout << "Allocate: " << MaxCount * sizeof( value_type ) << ", sizeof item= " << sizeof( value_type ) << std::endl;
+            cout << endl;
+#endif
+        }
 
-        if( buffer == nullptr )
+        if( m_buffer == nullptr )
         {
             throw std::bad_alloc();
         }
 
-        char* item_addr = buffer + offset;
-        offset += n * sizeof(value_type);
+        char* item_addr = m_buffer + m_offset;
+        m_offset += n * sizeof(value_type);
         return reinterpret_cast<pointer>( item_addr );
     }
 
@@ -55,20 +64,24 @@ struct Allocator
 #ifdef SHOW_DEBUG_INFO
         std::cout << __PRETTY_FUNCTION__ << "[n = " << n << "]" << std::endl;
 #endif
-        if( buffer != nullptr )
+        if( m_buffer != nullptr )
         {
-            offset -= n*sizeof(value_type);
-            if( offset == 0 )
+            m_count -= n;
+            if( m_count == 0 )
             {
                 std::free( p );
-                buffer = nullptr;
+                m_buffer = nullptr;
+#ifdef SHOW_DEBUG_INFO
+            cout << endl;
+            cout << "Deallocate p=" << p << std::endl;
+            cout << endl;
+#endif
             }
         }
-
     }
 
     template<typename U, typename ...Args>
-    void construct( U *p, Args &&...args )
+    void construct( U* p, Args &&...args )
     {
 #ifdef SHOW_DEBUG_INFO
         std::cout << __PRETTY_FUNCTION__ << std::endl;
@@ -76,17 +89,14 @@ struct Allocator
         new( p ) U( std::forward<Args>( args )... );
     }
 
-    void destroy( T *p )
+    void destroy( T* p )
     {
 #ifdef SHOW_DEBUG_INFO
         std::cout << __PRETTY_FUNCTION__ << std::endl;
 #endif
         p->~T();
     }
-
 };
-
-
 
 template< typename T, typename Allocator = std::allocator<T> >
 class List
@@ -147,28 +157,46 @@ public:
     };
 
 public:
-    List()
+    List(): m_head(nullptr), m_root( nullptr )
     {
-        m_head = nullptr;
-        m_root = nullptr;
     }
+
+    List( const List& other ): m_head(nullptr), m_root( nullptr )
+    {
+        for( auto v: other )
+        {
+            append(v);
+        }
+    }
+
+    List( List&& other ):  m_allocator( other.m_allocator ), m_head( other.m_head ), m_root( other.m_root )
+    {
+        other.m_head = nullptr;
+        other.m_root = nullptr;
+    }
+
     ~List()
     {
         auto node = m_root;
         while( node )
         {
             auto _node = node->m_next;
-            m_allocator.destroy(_node);
-            m_allocator.deallocate(_node,1);
+            if( _node )
+            {
+                m_allocator.destroy(_node);
+                m_allocator.deallocate(_node,1);
+            }
             node = _node;
         }
+        m_allocator.destroy(m_root);
+        m_allocator.deallocate(m_root,1);
     }
 
-    void append( const T& t )
+    template<typename... Args>
+    void append( Args&& ... args )
     {
-
         auto node = m_allocator.allocate(1);
-        m_allocator.construct(node, t);
+        m_allocator.construct(node, std::forward <Args>(args) ...);
 
         if( m_root == nullptr )
         {
@@ -181,22 +209,22 @@ public:
             m_head = m_head->m_next;
         }
     }
+
     T head() const
     {
         return  m_head;
     }
+
     Iterator begin() const
     {
         return Iterator( m_root );
     }
+
     Iterator end() const
     {
         return Iterator( nullptr );
     }
 };
-
-
-
 
 int fact( int n )
 {
@@ -239,8 +267,13 @@ int main( int, char *[] )
     // show container
     for( auto c : map2 )
     {
-        cout << to_string( c.first ) << " " << to_string( c.second ) << endl;
+        cout << c.first << " " << c.second << endl;
     }
+
+#ifdef SHOW_DEBUG_INFO
+    cout << "--------------------------------------------------" << endl;
+    cout << "list1" << endl;
+#endif
 
     // custom container
     auto list1 = List<int>{};
@@ -252,20 +285,45 @@ int main( int, char *[] )
     // show container
     for( auto c : list1 )
     {
-        cout << to_string( c ) << endl;
+        cout << c << endl;
     }
 
+    auto list2 = List<int, Allocator<int, COUNT_LIMIT>>{};
+    for( int i = 0; i < COUNT_LIMIT; i++ )
     {
-        auto list2 = List<int, Allocator<int, COUNT_LIMIT>>{};
-        for( int i = 0; i < COUNT_LIMIT; i++ )
-        {
-            list2.append( i );
-        }
+        list2.append( i );
+    }
 
-        for( auto c : list2 )
-        {
-            cout << to_string( c ) << endl;
-        }
+#ifdef SHOW_DEBUG_INFO
+    cout << "--------------------------------------------------" << endl;
+    cout << "list2" << endl;
+#endif
+
+    for( auto c : list2 )
+    {
+        cout << c << endl;
+    }
+
+#ifdef SHOW_DEBUG_INFO
+    cout << "--------------------------------------------------" << endl;
+    cout << "list3 - moved from list2 (list2 corrupted)" << endl;
+#endif
+
+    auto list3( std::move( list2 ) );
+    for( auto c : list3 )
+    {
+        cout << c << endl;
+    }   
+
+#ifdef SHOW_DEBUG_INFO
+    cout << "--------------------------------------------------" << endl;
+    cout << "list4 - copy of list1" << endl;
+#endif
+
+    auto list4(list1);
+    for( auto c : list4 )
+    {
+        cout << c << endl;
     }
 
     return 0;
